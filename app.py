0215fe0d348feaebba6f9729372c3bdf766f51a6
joybell <joybell@wemakeprice.com>
2020-03-18 04:20:54 +0900

    디렉토리 및 파일명 변경

---
+++ app.py
@@ -0,0 +1,85 @@
+#-*-coding: utf-8-*-
+from flask import Flask, request, jsonify
+from flask_restful import reqparse, abort, Api, Resource
+from datetime import datetime
+import module.service as service
+
+apiVersion = 'v1'
+
+app = Flask(__name__)
+api = Api(app)
+
+class Tasks(Resource):
+    def __init__(self):
+        self.service = service.Tasks()
+
+    # Get active tasks
+    def get(self):
+        try:
+            result = self.service.get_active_tasks()
+            return jsonify(result)
+        except Exception as e:
+            return {'error': str(e)}
+
+    # Create a new task
+    def post(self):
+        try:
+            body = request.get_json()
+            result = self.service.create_task(body['task_title'])
+            return jsonify(result)
+        except Exception as e:
+            return {'error': str(e)}            
+
+class Task(Resource):
+    def __init__(self):
+        self.service = service.Tasks()
+
+    # Get an active task
+    def get(self, task_id):
+        try:
+            result = self.service.get_active_task(task_id)
+            return jsonify(result)
+        except Exception as e:
+            return {'error': str(e)}
+
+    # Upate a task
+    def put(self, task_id):
+        try:
+            body = request.get_json()
+            result = self.service.update_task(task_id, body['task_title'])
+            return jsonify(result)
+        except Exception as e:
+            return {'error': str(e)}
+
+    # Delete a task
+    def delete(self, task_id):
+        try:
+            result = self.service.delete_task(task_id)
+            return jsonify(result)
+        except Exception as e:
+            return {'error': str(e)}
+
+class TaskStatus(Resource):
+    def __init__(self):
+        self.service = service.Tasks()
+
+    # Update a task status
+    def post(self, task_id, status):
+        try:
+            if status == 'close':
+                task_status = 'closed'
+            elif status == 'reopen':
+                task_status = 'active'
+
+            result = self.service.update_task_status(task_id, task_status)
+            return jsonify(result)
+        except Exception as e:
+            return {'error': str(e)} 
+
+
+api.add_resource(Tasks, '/rest/' + apiVersion + '/tasks')
+api.add_resource(Task, '/rest/' + apiVersion + '/tasks/<int:task_id>')
+api.add_resource(TaskStatus, '/rest/' + apiVersion + '/tasks/<int:task_id>/<string:status>')
+
+if __name__ == '__main__':
+    app.run(debug = True)
